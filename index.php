1363fcf1a5a732c56842028032de7f56632a0de0
Terence Lim <89091198+terlim-sfdc@users.noreply.github.com>
2022-09-08 11:37:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ while ($row = $result->fetch_assoc()){
 
 <center>
   <a href="index.php">View as list</a> &nbsp;&nbsp;&nbsp;&nbsp;
-  <a href="index.php?stacked">View stacked together</a>
+  <a href="index.php?stacked">View stacked on top of one another</a>
 </center><br /><br />
 
 <ul id="member-list" class="member-list<?php echo $is_stacked?' stacked':''; ?>">
